12bc227b43cafa9016f397753a2e560182db2570
Ubuntu <ubuntu@ip-172-31-25-226.us-west-2.compute.internal>
2013-07-19 00:28:30 +0000

    Added project-related contents

--- web.js
+++ web.js
@@ -2,6 +2,7 @@ var express = require('express');
 var fs = require ('fs');
 var htmlfile = "index.html";
 var app = express.createServer(express.logger());
+app.use(express.static(__dirname + '/images'));
 
 app.get('/', function(request, response) {
   var html = fs.readFileSync(htmlfile).toString();
